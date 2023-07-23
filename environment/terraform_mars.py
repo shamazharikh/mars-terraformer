cce0b021057dec26afc0d8b6646419b07f76309e
Mazhar <mazharshaikh94@gmail.com>
2023-07-23 15:24:52 +0000

    fix black

--- environment/terraform_mars.py
+++ environment/terraform_mars.py
@@ -6,9 +6,7 @@ class TerraformMars(gym.Env):
     metadata = {"render_modes": ["human", "rbg_array", "vector"], "render_fps": 4}
     def __init__(self, render_model=None, game_version="base", num_players=5):
         self.terraforming_rating = [20 for i in range(num_players)]
-        if game_version=="base":
-            board = 
 
     def get_board(self, board_path):
-
+        pass
 
