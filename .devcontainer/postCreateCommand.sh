243bd545d745ef702e2d2118dfb17fce8461d68d
Mazhar <mazharshaikh94@gmail.com>
2023-07-23 07:27:10 +0000

    add to git

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-USER_NAME=`cat ./user_variables/user_email`
+USER_NAME=`cat ./user_variables/user_name`
+USER_EMAIL=`cat ./user_variables/user_email`
 echo $USER_NAME
+echo $USER_EMAIL
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
