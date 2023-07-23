911a15f49a895c1a50200105debb4f103210e903
Mazhar <mazharshaikh94@gmail.com>
2023-07-23 22:42:19 +0000

    fix black

--- environment/terraform_mars.py
+++ environment/terraform_mars.py
@@ -1,12 +1,18 @@
 import gymnasium as gym
-import json 
+import json
 from pathlib import Path
+
 DATA_PATH = Path(__file__) / "data" / "terraformingmars"
+
+
 class TerraformMars(gym.Env):
-    metadata = {"render_modes": ["human", "rbg_array", "vector"], "render_fps": 4}
+    metadata = {
+        "render_modes": ["human", "rbg_array", "vector"],
+        "render_fps": 4,
+    }
+
     def __init__(self, render_model=None, game_version="base", num_players=5):
         self.terraforming_rating = [20 for i in range(num_players)]
 
     def get_board(self, board_path):
         pass
-
