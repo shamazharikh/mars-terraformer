b54fca4b6d64ba80c466627f318c6063deaa37c5
Mazhar <mazharshaikh94@gmail.com>
2023-07-25 04:10:26 +0000

    fix pylint

--- environment/terraform_mars.py
+++ environment/terraform_mars.py
@@ -1,6 +1,9 @@
-import gymnasium as gym
 import json
 from pathlib import Path
+from typing import Any, SupportsFloat
+
+import gymnasium as gym
+from gymnasium.
 
 DATA_PATH = Path(__file__) / "data" / "terraformingmars"
 
@@ -11,8 +14,17 @@ class TerraformMars(gym.Env):
         "render_fps": 4,
     }
 
-    def __init__(self, render_model=None, game_version="base", num_players=5):
+    def __init__(self, render_mode=None, game_version="base", num_players=5):
         self.terraforming_rating = [20 for i in range(num_players)]
+        self.render_mode = render_mode
+        self.game_version = game_version
 
-    def get_board(self, board_path):
+    def get_board(self, board_path: str):
+        board_data = json.read(board_path)
+        return board_data
+
+    def render(self) -> None:
+        pass
+
+    def step(self):
         pass
