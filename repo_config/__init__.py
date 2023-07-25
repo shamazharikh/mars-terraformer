f9f1b7b0dc0a19d764fe8d5dc9889fe699aee589
Mazhar <mazharshaikh94@gmail.com>
2023-07-25 09:49:22 +0000

    add config files

---
+++ repo_config/__init__.py
@@ -0,0 +1,6 @@
+"""
+Loads Config Classes and loads them with data from YAML files
+"""
+from .data_config import MarsConfig
+
+mars_config : MarsConfig = MarsConfig.from_yaml_file("/workspaces/mars-terraformer/config/mar_config.yml")
---
+++ repo_config/data_config.py
@@ -0,0 +1,23 @@
+"""
+class defines config dataclasses that can load yaml files and load config variables
+"""
+
+from dataclasses import dataclass
+
+from dataclass_wizard import YAMLWizard
+
+
+@dataclass
+class BoardConfig:
+    """
+    Define config variables dealing with board data
+    """
+    dir_path: str
+
+
+@dataclass
+class MarsConfig(YAMLWizard):
+    """
+    Define config variables for the terraforming mars game
+    """
+    board_config: BoardConfig
