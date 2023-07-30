67560c42d27b27d3dcf03e123ff3a482989d0208
Mazhar <mazharshaikh94@gmail.com>
2023-07-30 13:05:36 +0000

    add default github user

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-USER_NAME=`cat ./user_variables/user_name`
-USER_EMAIL=`cat ./user_variables/user_email`
+if [ -s /user_variables/user_email ]; then
+    USER_NAME=`cat ./user_variables/user_name`
+    USER_EMAIL=`cat ./user_variables/user_email`
+else
+    USER_NAME="Mazhar Shaikh"
+    USER_EMAIL="mazharshaikh94@gmail.com"
+fi
 echo $USER_NAME
 echo $USER_EMAIL
 git config --global user.email $USER_EMAIL
